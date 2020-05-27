6913bae49779a8fab16f3ba1b3e09289f583416e
Alexey Sobolevsky <sobolevsky@yandex-team.ru>
2020-05-27 19:15:27 +0300

    Chapter 3

--- ARPokerDice/ViewController.swift
+++ ARPokerDice/ViewController.swift
@@ -13,7 +13,11 @@ import ARKit
 class ViewController: UIViewController {
 
   // MARK: - Properties
-
+  var trackingStatus = "" {
+    didSet {
+      updateStatusLabel()
+    }
+  }
 
 
   // MARK: - Outlets
@@ -37,10 +41,9 @@ class ViewController: UIViewController {
   override func viewDidLoad() {
     super.viewDidLoad()
 
-    sceneView.delegate = self
-    sceneView.showsStatistics = true
-    let scene = SCNScene(named: "PokerDice.scnassets/SimpleScene.scn")!
-    sceneView.scene = scene
+    initSceneView()
+    initScene()
+    initARSession()
   }
 
   override func viewWillAppear(_ animated: Bool) {
@@ -58,7 +61,34 @@ class ViewController: UIViewController {
 
   // MARK: - Initialization
 
-  
+  private func initSceneView() {
+    sceneView.delegate = self
+    sceneView.showsStatistics = true
+  }
+
+  private func initScene() {
+
+  }
+
+  private func initARSession() {
+    guard ARWorldTrackingConfiguration.isSupported else {
+      print("*** ARConfig: AR World Tracking Not Supported")
+      return
+    }
+
+    let config = ARWorldTrackingConfiguration()
+    config.worldAlignment = .gravity
+    config.providesAudioData = false
+    sceneView.session.run(config)
+  }
+
+  // MARK: -
+
+  private func updateStatusLabel() {
+    DispatchQueue.main.async {
+      self.statusLabel.text = self.trackingStatus
+    }
+  }
 }
 
 
@@ -66,4 +96,39 @@ class ViewController: UIViewController {
 
 extension ViewController: ARSCNViewDelegate {
 
+  func sessionWasInterrupted(_ session: ARSession) {
+    trackingStatus = "AR Session Was Interrupted!"
+  }
+
+  func sessionInterruptionEnded(_ session: ARSession) {
+    trackingStatus = "AR Session Interruption Ended"
+  }
+
+  func session(_ session: ARSession, didFailWithError error: Error) {
+    trackingStatus = "AR Session Failure: \(error)"
+  }
+
+  func session(_ session: ARSession, cameraDidChangeTrackingState camera: ARCamera) {
+    switch camera.trackingState {
+    case .notAvailable:
+      trackingStatus = "Tacking: Not available!"
+
+    case .normal:
+      trackingStatus = ""
+
+    case .limited(let reason):
+      switch reason {
+      case .excessiveMotion:
+        trackingStatus = "Tracking: Limited due to excessive motion!"
+      case .insufficientFeatures:
+        trackingStatus = "Tracking: Limited due to insufficient features!"
+      case .initializing:
+        trackingStatus = "Tracking: Initializing..."
+      case .relocalizing:
+        trackingStatus = "Tracking: Relocalizing..."
+      @unknown default:
+        trackingStatus = "Tracking: Unknown..."
+      }
+    }
+  }
 }
